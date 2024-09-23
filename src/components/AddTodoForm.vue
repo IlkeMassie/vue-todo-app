0c80da640e9562b5672eebd71393c2a8c19f6ef5
Ilke Massie <ilke.massie@student.ucll.be>
2024-09-23 15:03:27 +0200

    changed the add-todo-form into component

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const newTodo = ref('');
+
+
+const emit = defineEmits(['addTodo'])
+
+
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    // Emit the 'addTodo' event to the parent with the newTodo value
+    emit('addTodo', newTodo.value);
+    // Clear the input field after adding a new todo
+    newTodo.value = '';
+  }
+};
+
+</script>
+
+
+<template>
+    <form @submit.prevent="addTodo">
+        <input class="textField" type="text" v-model="newTodo">
+        <button id="newTodo" class="button-1" type="submit">New todo</button>
+    </form>
+</template>
+
+<style scoped>
+
+
+#newTodo{
+    background-color: #DB5461;
+}
+
+</style>
